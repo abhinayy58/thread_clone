20d1a1f879870975ae13471b7b55d4e05b0c7716
Abhinay Yadav <abhinayy48@gmail.com.com>
2024-08-08 20:09:47 +0530

    first commit

---
+++ frontend/vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true, // also necessary
+  },
+  server: {
+		port: 3000,
+		// Get rid of the CORS error
+		proxy: {
+			"/api": {
+				target: "http://localhost:5000",
+				changeOrigin: true,
+				secure: false,
+			},
+		},
+	},
+})
